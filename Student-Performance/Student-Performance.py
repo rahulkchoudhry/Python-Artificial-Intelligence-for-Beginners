3ac6b65d6235fafbf569d0e400aedd0adc9f3412
Rahul Choudhry <rahulkchou@gmail.com>
2020-08-25 17:48:58 +0100

    Calculate accuracy scores to determine how successful the algorithm was. 2 or 3 deep decision trees are more accurate for this dataset to 69% hence the updated max_depth.

--- Student-Performance/Student-Performance.py
+++ Student-Performance/Student-Performance.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn import tree
+from sklearn.model_selection import cross_val_score
 
 d = pd.read_csv('student-por.csv', sep=';')
 
@@ -38,11 +39,17 @@ d_pass = d['pass']
 print("Passing: %d out of %d (%.2f%%)" % (np.sum(d_pass), len(d_pass), 100*float(np.sum(d_pass))/len(d_pass)))
 
 # Create a decision tree classifier
-t = tree.DecisionTreeClassifier(criterion="entropy", max_depth=5)
+t = tree.DecisionTreeClassifier(criterion="entropy", max_depth=3)
 t = t.fit(d_train_att, d_train_pass)
 
+# export graphviz .dot file for visualisation of the decision tree
 tree.export_graphviz(t, out_file='student-performance.dot', label="all", impurity=False, proportion=True,
                      feature_names=list(d_train_att), class_names=["fail", "pass"], filled=True, rounded=True)
 
+# calculate score of the tree we are using.
+# score = t.score(d_test_att, d_test_pass)
+# print(score)
 
-print(d)
+# calculate cross val score
+scores = cross_val_score(t, d_att, d_pass, cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
