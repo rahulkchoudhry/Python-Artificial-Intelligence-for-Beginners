2f9c5beafa28c2f793be79048050fc35c45bb171
Rahul Choudhry <rahulkchou@gmail.com>
2020-08-25 16:32:27 +0100

    Using pandas, create a new row for 'pass' which stores the calculated pass/fail based on G1 G2 G3. It then removes G1, G2 and G3 as these are no longer needed. The data is then converted into numerical data only using pd.get_dummies

--- Student-Performance/Student-Performance.py
+++ Student-Performance/Student-Performance.py
@@ -3,3 +3,16 @@ import pandas as pd
 d = pd.read_csv('student-por.csv', sep=';')
 
 print("Length: " + str(len(d)))
+
+# Add new label which calculates whether the student passed or failed and stores it under 'pass'
+
+d['pass'] = d.apply(lambda row: 1 if (row['G1'] + row['G2'] + row['G3']) >= 35 else 0, axis=1 )
+
+# After calculating the pass column, the 3 grade columns are no longer needed and can be dropped.
+d = d.drop(['G1', 'G2', 'G3'], axis=1)
+
+d = pd.get_dummies(d, columns=['sex', 'school', 'address', 'famsize', 'Pstatus', 'Mjob', 'Fjob',
+                               'reason', 'guardian', 'schoolsup', 'famsup', 'paid', 'activities',
+                               'nursery', 'higher', 'internet', 'romantic'])
+
+print(d)
